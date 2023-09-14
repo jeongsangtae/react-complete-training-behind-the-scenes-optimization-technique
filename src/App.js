efb70988ad994f863d322f47f423769a92d22609
jeongsangtae <wjdtkdxo55@naver.com>
2023-09-15 03:56:16 +0900

    React Complete Training Behind The Scenes Optimization Technique - component update check + virtual DOM understand

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("App RUNNING");
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagragh) => {
+      return !prevShowParagragh;
+    });
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
 }
